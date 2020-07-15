900b5ce124a9e239fa14a62be8c46519928758e8
btbunze <btbunze@live.unc.edu>
2020-07-14 23:30:57 -0400

    added image at the beginning of index.js

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,7 +3,6 @@ export class Layout extends React.Component {
     render() {
         return (
             <>
-                <link rel="image_src" href="https://res.cloudinary.com/dicfhqxoo/image/upload/v1594781483/thumbnails/Screenshot_67_l9wiq3.png" />
                 {this.props.children}
             </>
         )
